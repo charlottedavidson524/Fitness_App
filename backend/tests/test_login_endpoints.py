366505e456b153000e7beff9bc6c6c0a54f3fe80
charlottedavidson524 <charlottedavidson256@gmail.com>
2025-12-04 11:56:56 +0000

    Testing for login endpoints.

---
+++ backend/tests/test_login_endpoints.py
@@ -0,0 +1,51 @@
+import pytest
+from backend.app import app
+
+@pytest.fixture
+def client():
+    # Turn on Flask testing mode.
+    # Creates a Flask test client.
+    # Simulates HTTP requests to Flask app in memory without running server.
+    app.config["TESTING"] = True
+    with app.test_client() as client:
+        yield client
+
+def test_nonexistent_user(client):
+    response = client.post("/login", json = {
+        "username": "ghost",
+        "password": "whatever"
+    })
+    assert response.status_code == 404
+    assert "not found" in response.json["error"].lower()
+
+def test_login_incorrect_password(client):
+    # Create user
+    client.post("/register", json = {
+        "username": "john",
+        "password": "password",
+        "email": "john@example.com"
+    })
+
+    # Try logging in with wrong password
+    response = client.post("/login", json = {
+        "username": "john",
+        "password": "wrongpassword"
+    })
+
+    assert response.status_code == 401
+    assert "incorrect" in response.json["error"].lower()
+
+def test_login_success(client):
+    client.post("/register", json = {
+        "username": "sam",
+        "password": "password123",
+        "email": "sam@email.com"
+    })
+
+    response = client.post("/login", json = {
+        "username": "sam",
+        "password": "password123"
+    })
+
+    assert response.status_code == 200
+    assert "token" in response.json 
